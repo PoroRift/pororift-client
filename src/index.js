05b52b978838befe588902ebceb710cce73f090a
bigslikky <37082165+bigslikky@users.noreply.github.com>
2018-08-26 20:20:55 -0700

    okay this should work

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Background from "./components/Background.js";
 import Searchbar from "./components/Searchbar.js";
+import Topnav from "./components/Topnav.js";
 import './index.css';
 
 const Index = () => {
 	return  <div className= "Index">
 					<Background />
-					
+					<Topnav />
 					<Searchbar />
 					</div>;
 };
