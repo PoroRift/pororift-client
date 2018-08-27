8e374e5c72b86555c50f58766d72754a2480cfc1
bigslikky <37082165+bigslikky@users.noreply.github.com>
2018-08-26 19:49:50 -0700

    fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Background from "./components/Background.js";
-import Topnav from "./components/Topnav.js";
 import Searchbar from "./components/Searchbar.js";
 import './index.css';
 
 const Index = () => {
 	return  <div className= "Index">
 					<Background />
-					<Topnav />
+					
 					<Searchbar />
 					</div>;
 };
