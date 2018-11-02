85ab63056492a492c1cd415a74423e569ef638ea
Michael Handria <michaelhandria@michaels-mbp.lan>
2018-11-02 13:05:45 -0700

    [feat] added Material Modules into application

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
+const MaterialModule = [
+  BrowserAnimationsModule,
+  MatIconModule,
+];
+
 @NgModule({
   declarations: [],
   imports: [
+    MaterialModule,
     CommonModule
+  ],
+  exports: [
+    MaterialModule
   ]
 })
 export class SharedModule { }
