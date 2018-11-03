597115983931d53f39c245a40e402921bce33301
Michael Handria <michaelhandria@gmail.com>
2018-11-02 17:30:34 -0700

    [feat] added app title and search bar to nav bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialModule = [
   BrowserAnimationsModule,
   MatIconModule,
+  MatToolbarModule
 ];
 
 @NgModule({
