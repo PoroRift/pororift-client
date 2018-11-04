78a3d09c1507ad151a7c1a40a8a1a17e9298945c
Michael Handria <michaelhandria@michaels-mbp.lan>
2018-11-04 11:57:38 -0800

    [feat] added open & close functions to sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public opened = true;
+
+  public toggleSideNavEvent(): void {
+    console.log(this.opened);
+    this.opened = !this.opened;
+  }
 }
--- src/app/layout/navigation-bar/navigation-bar.component.ts
+++ src/app/layout/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
+  @Output() toggleSideNav = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleSideNavEvent(): void {
+    this.toggleSideNav.emit(null);
   }
 
 }
