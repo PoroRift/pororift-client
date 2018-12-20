d04409515f353db8fcaa6b736c05dc7e28263a6f
Michael Handria <michaelhandria@michaels-mbp.lan>
2018-12-20 00:42:07 -0800

    [feat] added new summoner tile detail component

---
+++ src/app/match-viewer-app/summoner-tile-detail/summoner-tile-detail.component.ts
@@ -0,0 +1,18 @@
+import { SummonerDetail } from './../match-viewer-model';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-summoner-tile-detail',
+  templateUrl: './summoner-tile-detail.component.html',
+  styleUrls: ['./summoner-tile-detail.component.scss']
+})
+export class SummonerTileDetailComponent implements OnInit {
+
+  @Input() summonerDetail: SummonerDetail;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
