bc11827073b720df202b130b65dee823b0ecd9e7
Cherie Woo <cli@cwoo-mv-ltmp-x.local>
2018-12-23 22:44:56 -0800

    Removed banned-display component, kept all logic in team-display

--- src/app/match-viewer-app/match-viewer-app.module.ts
+++ src/app/match-viewer-app/match-viewer-app.module.ts
@@ -6,15 +6,13 @@ import { CommonModule } from '@angular/common';
 import { TeamDisplayComponent } from './team-display/team-display.component';
 import { SummonerTileComponent } from './summoner-tile/summoner-tile.component';
 import { SharedModule } from '../shared/shared.module';
-import { BannedDisplayComponent } from './banned-display/banned-display.component';
 
 @NgModule({
   declarations: [
     TeamDisplayComponent,
     MatchViewerLayoutComponent,
     MatchDisplayComponent,
-    SummonerTileComponent,
-    BannedDisplayComponent
+    SummonerTileComponent
   ],
   imports: [
     CommonModule,
