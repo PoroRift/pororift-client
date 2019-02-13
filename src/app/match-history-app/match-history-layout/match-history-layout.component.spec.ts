b873d5b1c7b768de698624db8046850ee14cb638
Cherie Woo <cheriejawoo@yahoo.com>
2019-02-13 09:25:16 -0800

    [chore] match-history defautl tests

--- src/app/match-history-app/match-history-layout/match-history-layout.component.spec.ts
+++ src/app/match-history-app/match-history-layout/match-history-layout.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MatchHistoryLayoutComponent } from './match-history-layout.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
 
 describe('MatchHistoryLayoutComponent', () => {
   let component: MatchHistoryLayoutComponent;
@@ -8,6 +9,7 @@ describe('MatchHistoryLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       declarations: [ MatchHistoryLayoutComponent ]
     })
     .compileComponents();
